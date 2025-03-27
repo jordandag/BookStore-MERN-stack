c28f62693cc814d0c8f100224bc669bbb3724c77
Dagemawi Bmeskel <dive.dag.20219@gmail.com>
2025-03-27 08:08:15 +0300

    finished the backend and moving on to the frontend

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Middleware for handling CORS POLICY
 // Option 1: Allow All Origins with Deault of cors(*)
-app.use(cors());
+// app.use(cors());
 
 // Option 2: Allow Custom Origins
 app.use(
