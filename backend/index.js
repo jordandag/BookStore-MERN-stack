99113f4bc736bb01ee036e4a92d8d2a529a71975
Dagemawi Bmeskel <dive.dag.20219@gmail.com>
2025-03-27 08:07:38 +0300

    finished the backend and moving on to the frontend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import booksRoute from "./routes/booksRoute.js";
 
@@ -8,6 +9,19 @@ const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Deault of cors(*)
+app.use(cors());
+
+// Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   return res.status(234).send(`Welcom To MERN Stack Tutorial`);
